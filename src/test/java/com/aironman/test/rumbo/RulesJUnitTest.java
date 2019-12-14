fa5e14cda2ab7f733c492214c6c2e351af7f6ead
aironman <alonsoir@gmail.com>
2019-12-14 13:10:21 +0100

    added necessary rules to be applied in all cases.

--- src/test/java/com/aironman/test/rumbo/RulesJUnitTest.java
+++ src/test/java/com/aironman/test/rumbo/RulesJUnitTest.java
@@ -23,75 +23,69 @@ import com.aironman.test.rumbo.model.Order;
 @RunWith(Arquillian.class)
 public class RulesJUnitTest {
 
-    public RulesJUnitTest() {
-    }
+	public RulesJUnitTest() {
+	}
 
-    @Deployment
-    public static JavaArchive createDeployment() {
+	@Deployment
+	public static JavaArchive createDeployment() {
 
-        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-        System.out.println(jar.toString(true));
-        return jar;
-    }
+		JavaArchive jar = ShrinkWrap.create(JavaArchive.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+		System.out.println(jar.toString(true));
+		return jar;
+	}
 
-    @Inject
-    @KSession
-    private KieSession kSession;
+	@Inject
+	@KSession
+	private KieSession kSession;
 
-    @Test
-    public void hello() {
-        Assert.assertNotNull(kSession);
-        kSession.insert("Hi There From Test!");
-        Assert.assertEquals(1, kSession.fireAllRules());
-    }
-    /*
-    @Test
-    public void should_apply_rules_to_order1_exempted_imported_not_imported() {
-    	//GIVEN
-    	Assert.assertNotNull(kSession);
-    	final Order order1 = Utilities.createOrder1();
-    	//final List<Product> products = order1.getProducts();
-    	//products.forEach(System.out::println);
-    	//WHEN
-    	kSession.insert(order1);
+	@Test
+	public void hello() {
+		Assert.assertNotNull(kSession);
+		kSession.insert("Hi There From Test!");
+		Assert.assertEquals(1, kSession.fireAllRules());
+	}
 
-    	//THEN
-    	Assert.assertEquals(1, kSession.fireAllRules());
-    	Assert.assertEquals("Shoud be equals...",29.83d, order1.getTotalPrize(),0d);
-    	Assert.assertEquals("Shoud be equals...",1.50d, order1.getTotalTaxes(),0d);
-    }
-    */
-    
-    @Test
-    public void should_apply_rules_to_order2_exempetd_imported_not_imported() {
-    	//GIVEN
-    	Assert.assertNotNull(kSession);
-    	final Order order2 = Utilities.createOrder2();
-    	//final List<Product> products = order2.getProducts();
-    	//products.forEach(System.out::println);
-    	//WHEN
-    	kSession.insert(order2);
-    	kSession.fireAllRules();
-    	//THEN
-    	Assert.assertEquals("Shoud be equals...",65.15d, order2.getTotalPrize(),0d);
-    	Assert.assertEquals("Shoud be equals...",7.65d, order2.getTotalTaxes(),0d);
-    }
-    /*
-    @Test
-    public void should_apply_rules_to_order3_exented_and_imported_not_exempted_imported_exempted_not_imported_not_exempted_not_imported() {
-    	//GIVEN
-    	Assert.assertNotNull(kSession);
-    	final Order order3 = Utilities.createOrder3();
-    	//final List<Product> products = order3.getProducts();
-    	//products.forEach(System.out::println);
-    	//WHEN
-    	kSession.insert(order3);
+	@Test
+	public void should_apply_rules_to_order1_exempted_imported_not_imported() {
+		// GIVEN
+		Assert.assertNotNull(kSession);
+		final Order order1 = Utilities.createOrder1();
+		// WHEN
+		kSession.insert(order1);
+		kSession.fireAllRules();
+		// THEN
+		System.out.println("order1 after apply rules: " + order1.toString());
+		Assert.assertEquals("Shoud be equals...", 29.83d, order1.getTotalPrize(), 0.05d);
+		Assert.assertEquals("Shoud be equals...", 1.50d, order1.getTotalTaxes(), 0.05d);
+	}
 
-    	//THEN
-    	Assert.assertEquals(1, kSession.fireAllRules());
-    	Assert.assertEquals("Shoud be equals...",74.68d, order3.getTotalPrize(),0d);
-    	Assert.assertEquals("Shoud be equals...",6.70d, order3.getTotalTaxes(),0d);
-    }
-    */
+	@Test
+	public void should_apply_rules_to_order2_exempetd_imported_not_imported() {
+		// GIVEN
+		Assert.assertNotNull(kSession);
+
+		final Order order2 = Utilities.createOrder2();
+		// WHEN
+
+		kSession.insert(order2);
+		kSession.fireAllRules();
+		// THEN
+		System.out.println("order2 after apply rules: " + order2.toString());
+		Assert.assertEquals("Shoud be equals...", 65.15d, order2.getTotalPrize(), 0.05d);
+		Assert.assertEquals("Shoud be equals...", 7.65d, order2.getTotalTaxes(), 0.05d);
+	}
+
+	@Test
+	public void should_apply_rules_to_order3_exented_and_imported_not_exempted_imported_exempted_not_imported_not_exempted_not_imported() {
+		// GIVEN 
+		Assert.assertNotNull(kSession); 
+		final Order order3 =Utilities.createOrder3(); 
+		kSession.insert(order3);
+		kSession.fireAllRules();
+		// THEN 
+		System.out.println("order3 after apply rules: " + order3.toString());
+		Assert.assertEquals("Shoud be equals...", 74.68d, order3.getTotalPrize(), 0.05d);
+		Assert.assertEquals("Shoud be equals...", 6.70d, order3.getTotalTaxes(), 0.05d);
+	}
+
 }
